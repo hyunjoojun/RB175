9b2230e02ef2fe6d93222a3b04a26eaa4c4d192b
Hyunjoo Jun <hjj102088@gmail.com>
2023-08-04 13:59:01 -0400

    redirect

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -15,8 +15,10 @@ end
 get "/chapters/:number" do
   number = params[:number].to_i
   chapter_name = @contents[number - 1]
-  @title = "Chapter #{number}: #{chapter_name}"
 
+  redirect "/" unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_name}"
   @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
@@ -28,4 +30,8 @@ helpers do
       "<p>#{paragraph}</p>"
     end.join
   end
+end
+
+not_found do
+  redirect "/"
 end
